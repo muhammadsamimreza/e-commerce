2e2fb4803fd1915925ad99e9cdae9a44f5c56db9
muhammadsamimreza <158745217+muhammadsamimreza@users.noreply.github.com>
2024-04-02 01:48:11 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,40 +1,63 @@
 let product =[{
         
         id: '0',
-        image: "./images/mobile.jpg",
-        title: 'shoe',
+        image: "./images/01.png",
+        title: ' Oleves Watch',
         price: 300,
-        catagory: 'sports'
+        
     },
    {
         id: '1',
-        title: 'bag',
+        image: "./images/02.png",
+        title: 'Oleves Watch',
         price: 200,
         catagory: 'sports'},
    
     {
         id: '2',
-        title: 'Tie',
+        image: "./images/03.png",
+        title: 'Oleves Watch',
         price: 500,
         catagory: 'wear'},
 
     { 
         id: '3',
-        title: 'shirt',
+        image: "./images/04.png",
+        title: 'Oleves Watch',
         price: 1000,
-        catagory: 'cloths'}
+        catagory: 'cloths'},
+
+        { 
+            id: '3',
+            image: "./images/05.png",
+            title: 'Oleves Watch',
+            price: 1000,
+            catagory: 'cloths'},
+            { 
+                id: '3',
+                image: "./images/07.png",
+                title: 'Oleves Watch',
+                price: 1000,
+                catagory: 'cloths'}            
 ]
 
 let data=''
 for(let pro in product){
     const sinPro = product[pro]
 
-    data+=`<div class="text-center"><div class="border-2"> <img src="./images/mobile.jpg"> 
-    <div class="text-xl font-bold"> Id:${sinPro.id}</div>
-    <div class="text-xl font-bold"> Name:${sinPro.title}</div>
-    <div class="text-sm"> Price:$${sinPro.price}</div>
-    <div class="text-sm"> catagory:${sinPro.catagory}</div></div></div>`
+    data+=`
+    <div class="text-center mx-auto">
+            <div class="border-2 space-y-1 border-[#f59e0b] py-3">
+                 <div class=" flex justify-center"><img class="w-44 h-36" src="${sinPro.image}"></div> 
+             
+            <div class="text-[16px] font-semibold">${sinPro.title}</div>
+            <div class="text-red-500 font-semibold">$${sinPro.price}</div>
+         
+         <div> <button class="bg-[#f59e0b] w-[90%] rounded-md text-blue-500 font-bold py-1 hover:bg-green-500 hover:text-red-700">Add to cart</button></div>
+        </div>
+    </div>`
 
+    
     console.log(data)
 }
 document.getElementById('pbox').innerHTML=data;
